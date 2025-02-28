e002b8e2395f5f479ab3218473fed171e636559c
Irvin-Silva <irvin.aurelio27@gmail.com>
2025-02-28 13:18:08 -0500

    add defer functionality with checkbox in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { TaskComponent } from "./dashboard/task/task.component";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,5 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'demo_nuevas_caracteristicas';
+  isCheckedDefender = signal(false);
 }
